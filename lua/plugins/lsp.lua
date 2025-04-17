1e6cd7d0ac4aef467cb392aa936a2f150076cf3d
stucodegreen <m.kuznecovas@gmail.com>
2025-04-17 23:51:38 +0300

    added lsp

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,44 @@
+-- lua/plugins/lsp.lua
+return {
+  {
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v3.x",
+    dependencies = {
+      -- LSP Support
+      "neovim/nvim-lspconfig",
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+
+      -- Autocompletion
+      "hrsh7th/nvim-cmp",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      local lsp = require("lsp-zero")
+
+      lsp.extend_lspconfig()
+
+      -- Common capabilities
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      -- HTML LSP
+      require("lspconfig").html.setup({
+        capabilities = capabilities,
+        on_attach = function(client, bufnr)
+          -- Custom html LSP on_attach logic
+        end,
+      })
+
+      -- TypeScript LSP (tsserver or tsserver from mason)
+      require("lspconfig").ts_ls.setup({
+        capabilities = capabilities,
+        on_attach = function(client, bufnr)
+          client.server_capabilities.documentFormattingProvider = false
+          -- Add other custom logic here
+        end,
+      })
+
+      lsp.setup()
+    end,
+  },
+}
