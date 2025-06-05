a3290464cc45276dde048edbb21579398f90d7e5
stucodegreen <m.kuznecovas@gmail.com>
2025-06-05 07:53:39 +0300

    added prettier

---
+++ lua/plugins/prettier.lua
@@ -0,0 +1,37 @@
+return {
+  "nvimtools/none-ls.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local null_ls = require("null-ls")
+
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.prettier.with({
+          filetypes = {
+            "javascript",
+            "typescript",
+            "javascriptreact",
+            "typescriptreact",
+            "json",
+            "html",
+            "css",
+            "scss",
+            "markdown",
+          },
+        }),
+        null_ls.builtins.formatting.stylua,
+      },
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = bufnr })
+            end,
+          })
+        end
+      end,
+    })
+  end,
+}
