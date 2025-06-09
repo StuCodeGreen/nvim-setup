eb9e87fd4f7aa14892c9d6f6a193a138333bffbe
stucodegreen <m.kuznecovas@gmail.com>
2025-06-09 12:56:30 +0300

    prettier config

--- lua/plugins/prettier.lua
+++ lua/plugins/prettier.lua
@@ -1,37 +1,38 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.prettier.with({
-          filetypes = {
-            "javascript",
-            "typescript",
-            "javascriptreact",
-            "typescriptreact",
-            "json",
-            "html",
-            "css",
-            "scss",
-            "markdown",
-          },
-        }),
-        null_ls.builtins.formatting.stylua,
-      },
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({ bufnr = bufnr })
-            end,
-          })
-        end
-      end,
-    })
-  end,
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.prettier.with({
+					filetypes = {
+						"javascript",
+						"typescript",
+						"javascriptreact",
+						"typescriptreact",
+						"json",
+						"html",
+						"css",
+						"scss",
+						"markdown",
+					},
+					prefer_local = "node_modules/.bin",
+				}),
+				null_ls.builtins.formatting.stylua,
+			},
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ bufnr = bufnr })
+						end,
+					})
+				end
+			end,
+		})
+	end,
 }
