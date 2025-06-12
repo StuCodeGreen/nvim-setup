081fe3f5b183712bc0838efcff35f4868d9c17b7
stucodegreen <m.kuznecovas@gmail.com>
2025-06-12 08:29:03 +0300

    added indent guides and save undotree history

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,9 +1,24 @@
 return {
-  {
-    "mbbill/undotree",
-    cmd = "UndotreeToggle", -- Lazy-load when you run the command
-    keys = {
-      { "<leader>u", vim.cmd.UndotreeToggle, desc = "Toggle Undotree" },
-    },
-  },
+	"mbbill/undotree",
+	cmd = "UndotreeToggle",
+	keys = {
+		{ "<leader>u", vim.cmd.UndotreeToggle, desc = "Toggle Undotree" },
+	},
+	config = function()
+		vim.cmd([[
+            if has("persistent_undo")
+
+            let target_path = expand('~/.undotree/')
+
+            " create the directory and any parent directories
+            " if the location does not exist.
+            if !isdirectory(target_path)
+            call mkdir(target_path, "p", 0700)
+            endif
+
+            let &undodir=target_path
+            set undofile
+            endif
+            ]])
+	end,
 }
