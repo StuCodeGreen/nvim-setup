0bd2dd712b40d0d1e1427b3831f4107720ea4dd5
stucodegreen <m.kuznecovas@gmail.com>
2025-06-18 15:53:56 +0300

    disable formatting on save

--- lua/plugins/prettier.lua
+++ lua/plugins/prettier.lua
@@ -39,7 +39,7 @@ return {
 					vim.api.nvim_create_autocmd("BufWritePre", {
 						buffer = bufnr,
 						callback = function()
-							vim.lsp.buf.format({ bufnr = bufnr })
+							-- vim.lsp.buf.format({ bufnr = bufnr })
 						end,
 					})
 				end
