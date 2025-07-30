fcb221bac155d45dc860ba0a522a92e1df9c7af5
stucodegreen <m.kuznecovas@gmail.com>
2025-07-30 10:21:59 +0300

    git config update

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,62 +1,48 @@
 return {
-	-- Git signs in the gutter
-	{
-		"lewis6991/gitsigns.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		opts = {
-			signs = {
-				add = { text = "+" },
-				change = { text = "~" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-			},
-			current_line_blame = true, -- optional
+  -- Git signs in the gutter
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      signcolumn = true,
+      numhl = true,
+      linehl = true,
 
-			on_attach = function(bufnr)
-				local gs = package.loaded.gitsigns
-				local function map(mode, lhs, rhs, desc)
-					vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
-				end
+      signs = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+      },
 
-				-- Navigation between hunks
-				map("n", "]c", function()
-					if vim.wo.diff then
-						return "]c"
-					end
-					vim.schedule(gs.next_hunk)
-					return "<Ignore>"
-				end, "Next Git hunk")
+      current_line_blame = true,
 
-				map("n", "[c", function()
-					if vim.wo.diff then
-						return "[c"
-					end
-					vim.schedule(gs.prev_hunk)
-					return "<Ignore>"
-				end, "Previous Git hunk")
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+        local function map(mode, lhs, rhs, desc)
+          vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+        end
 
-				-- Actions
-				map("n", "<leader>hs", gs.stage_hunk, "Stage hunk")
-				map("n", "<leader>hu", gs.undo_stage_hunk, "Undo stage hunk")
-				map("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
-				map("n", "<leader>hR", gs.reset_buffer, "Reset buffer")
-				map("n", "<leader>hp", gs.preview_hunk, "Preview hunk")
-				map("n", "<leader>hb", function()
-					gs.blame_line({ full = true })
-				end, "Blame line (full)")
-				map("n", "<leader>hd", gs.diffthis, "Diff this file")
-			end,
-		},
-	},
+        -- Actions
+        map("n", "<leader>hs", gs.stage_hunk, "Stage hunk")
+        map("n", "<leader>hu", gs.undo_stage_hunk, "Undo stage hunk")
+        map("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
+        map("n", "<leader>hR", gs.reset_buffer, "Reset buffer")
+        map("n", "<leader>hp", gs.preview_hunk_inline, "Preview hunk inline")
+        map("n", "<leader>hd", gs.diffthis, "Diff this file")
+      end,
+    },
+  },
 
-	-- LazyGit integration
-	{
-		"kdheepak/lazygit.nvim",
-		cmd = "LazyGit",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		keys = {
-			{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "Open LazyGit" },
-		},
-	},
+  -- LazyGit integration
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = "LazyGit",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "Open LazyGit" },
+    },
+  },
 }
+
